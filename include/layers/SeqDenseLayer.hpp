c29db5c001a3a05a14b57d643a5b84038c132bd8
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-05-31 21:39:51 +0530

    Create class definition for sequential layers

---
+++ include/layers/SeqDenseLayer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "core/ILayer.hpp"
+
+class SeqDenseLayer : public ILayer
+{
+private:
+    std::vector<std::vector<float>> weightMatrix;
+    std::vector<float> bias;
+    unsigned int neuronCount;
+    unsigned int inputCount;
+
+public:
+    SeqDenseLayer(unsigned int neuronCount, unsigned int inputCount);
+    std::vector<float> forward(std::vector<float> &input) override;
+    std::vector<float> backward(std::vector<float> gradOutput) override;
+    void printLayer(int layerId = -1);
+    void update(float lr) override;
+    void zero_grad() override;
+};
