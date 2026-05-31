92986699c3ff3ed7b5d135ea2766339a013248ef
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-05-31 18:20:44 +0530

    Remove templates to use float everywhere

--- include/core/ILayer.hpp
+++ include/core/ILayer.hpp
@@ -2,12 +2,11 @@
 
 #include <vector>
 
-template <typename T>
 class ILayer
 {
 public:
-    virtual std::vector<T> forward(std::vector<T> &input) = 0;
-    virtual std::vector<T> backward(std::vector<T> grad_output) = 0;
-    virtual void update(T lr) = 0;
+    virtual std::vector<float> forward(std::vector<float> &input) = 0;
+    virtual std::vector<float> backward(std::vector<float> grad_output) = 0;
+    virtual void update(float lr) = 0;
     virtual void zero_grad() = 0;
 };
--- include/core/INeuralNetwork.hpp
+++ include/core/INeuralNetwork.hpp
@@ -2,13 +2,12 @@
 
 #include <vector>
 
-template <typename T>
 class INeuralNetwork
 {
 public:
     virtual ~INeuralNetwork() = default;
-    virtual std::vector<T> forward(const std::vector<T> &input) = 0;
-    virtual std::vector<T> backward(const std::vector<T> &grad_output) = 0;
-    virtual void update(T learning_rate) = 0;
+    virtual std::vector<float> forward(const std::vector<float> &input) = 0;
+    virtual std::vector<float> backward(const std::vector<float> &grad_output) = 0;
+    virtual void update(float learning_rate) = 0;
     virtual void zero_grad() = 0;
 };
