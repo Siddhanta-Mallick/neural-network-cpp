0c500b4120da8c6f75b3409c8cc1a64fe728ac91
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-06-01 18:23:02 +0530

    Add ReLu and Sigmoid activation functions

---
+++ include/core/ActivationFunctions.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+
+class Activation
+{
+public:
+    static float ReLU(float y)
+    {
+        return std::fmax(y, 0.0f);
+    }
+    static float Sigmoid(float y)
+    {
+        if (y >= 0)
+        {
+            float z = exp(-y);
+            return 1 / (1 + z);
+        }
+        else
+        {
+            float z = exp(y);
+            return z / (1 + z);
+        }
+    }
+};
